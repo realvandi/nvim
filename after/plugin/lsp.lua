1e1b2e42cda1465b63eaf1b93b472fb087658b5d
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-13 14:26:27 +0200

    feat: Create lsp commands

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,7 +1,6 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-lsp.setup()
 
 lsp.ensure_installed({
 	'tsserver',
@@ -22,3 +21,14 @@ require('mason-lspconfig').setup({
 		end,
 	},
 })
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+lsp.setup()
