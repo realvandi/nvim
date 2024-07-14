0653bd2d2c7350b830614d093442db06839b7731
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-14 09:10:03 +0200

    feat: Remap <C-\\><C-n> in terminal to exit terminal mode to escape

--- lua/sudohaxe/remap.lua
+++ lua/sudohaxe/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = " "
+
+-- Explorer remap
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Terminal mode escape
+vim.api.nvim_set_keymap('t','<ESC>','<C-\\><C-n>',{noremap = true})
