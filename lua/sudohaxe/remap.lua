07493042a76bfc3481d67213ba27013e5b648f82
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-15 12:21:38 +0200

    feat: Add Trey Bastian's Tabs, and Buffer keymaps

--- lua/sudohaxe/remap.lua
+++ lua/sudohaxe/remap.lua
@@ -5,3 +5,13 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Terminal mode escape
 vim.api.nvim_set_keymap('t','<ESC>','<C-\\><C-n>',{noremap = true})
+
+-- Trey_bastian's Keymaps
+vim.api.nvim_set_keymap("v", "<Tab>", ">gv", {noremap = true})
+vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Tab>", "v><C-\\><C-N>", {noremap = tr})
+vim.api.nvim_set_keymap("n", "<S-Tab>", "v<<C-\\><C-N>", {noremap = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "<C-\\><C-N>v<<C-\\><C-N>^i", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>bn", "<CMD>bn<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>bp", "<CMD>bp<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>bd", "<CMD>bd<CR>", {noremap = true})
