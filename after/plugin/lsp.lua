00f50dd18c2c501f631124b4e51b59af5b8f65e2
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-20 03:21:53 +0200

    feat: Remove deprecated LSP commands, change initialization notes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,15 +2,17 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.ensure_installed({
-	'tsserver',
-	'eslint',
-	'sumneko_lua',
-	'rust_analyzer',
-})
+-- Deprecated LSP configuration
+-- lsp.ensure_installed({
+-- 	'tsserver',
+-- 	'eslint',
+-- 	'sumneko_lua',
+-- 	'rust_analyzer',
+-- })
 
 
 require('mason').setup({})
+
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here 
 	-- with the ones you want to install
