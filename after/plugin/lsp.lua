f337900225d496341560772b9259a699e61dc183
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-22 05:37:51 +0200

    feat: Configure autoformat on save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,6 +2,77 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
+require('mason').setup({})
+
+require('mason-lspconfig').setup({
+    ensure_installed = {'tsserver', 'rust_analyzer', 'eslint'},
+    handlers = { function(server_name)
+        require('lspconfig')[server_name].setup({
+            on_attach = function(client, bufnr)
+                    local opts = {buffer = bufnr, remap = false}
+                    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+                    -- Call the formatting function
+                    lsp_format_on_save(bufnr)
+                end,
+            })
+        end,
+    },
+})
+
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local lsp_format_on_save = function(bufnr)
+    vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({
+                filter = function(client)
+                    return client.name == "null-ls"
+                end,
+                bufnr = bufnr,
+            })
+        end,
+    })
+end
+
+lsp.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+    -- Call the formatting function
+       lsp_format_on_save(bufnr)
+end)
+
+lsp.setup()
+
+-- Configure null-ls
+local null_ls = require('null-ls')
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.prettier, -- Add your preferred formatters here
+        null_ls.builtins.diagnostics.eslint, -- Add your preferred linters here
+    },
+    on_attach = function(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+            lsp_format_on_save(bufnr)
+        end
+    end
+})
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
 -- Deprecated LSP configuration
 -- lsp.ensure_installed({
 -- 	'tsserver',
--- lua/sudohaxe/packer.lua
+++ lua/sudohaxe/packer.lua
@@ -137,6 +137,10 @@ return require('packer').startup(function(use)
             {'nvim-lua/plenary.nvim'},
         }
     }
+    use {
+        'jose-elias-alvarez/null-ls.nvim',
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
 
 end)
 
