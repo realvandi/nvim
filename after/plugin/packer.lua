8a572746559954c4517fca9ce4d516456a874d2d
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-24 09:13:48 +0200

    feat: Install flash for packer

---
+++ after/plugin/packer.lua
@@ -0,0 +1,16 @@
+local flash = require("flash")
+
+vim.keymap.set("n", "s", function() flash.jump() end, { desc = "Flash" })
+vim.keymap.set("x", "s", function() flash.jump() end, { desc = "Flash" })
+vim.keymap.set("o", "s", function() flash.jump() end, { desc = "Flash" })
+
+vim.keymap.set("n", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("x", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set("o", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
+
+vim.keymap.set("o", "r", function() flash.remote() end, { desc = "Remote Flash" })
+
+vim.keymap.set("o", "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
+vim.keymap.set("x", "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
+
+vim.keymap.set("c", "<c-s>", function() flash.toggle() end, { desc = "Toggle Flash Search" })
--- lua/sudohaxe/packer.lua
+++ lua/sudohaxe/packer.lua
@@ -145,5 +145,14 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
+    use {
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        opts = {}, -- Add any additional configuration options here
+        config = function()
+            require('flash').setup({})
+        end
+    }
+
 end)
 
